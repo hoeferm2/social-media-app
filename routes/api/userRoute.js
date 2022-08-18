09ee4b7cbd1fce3992600704e7f2339d05d90344
Hoefer <ningboninja@gmail.com>
2022-08-17 22:02:42 -0700

    working on routes and controllers

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -6,13 +6,22 @@ const {
     createUser,
     updateUser,
     deleteUser,
+    //TODO:
+    getAllFriends,
     addFriend,
     deleteFriend,
 } = require('../../controllers/users-controllers')
 
+//gets all users / allows you to create a new user
+
 router.route('/').get(getAllUser).post(createUser)
 
+// get user based on id // update based on user id // delete based on user id
+
 router.route('/:userId').get(getOneUser).put(updateUser).delete(deleteUser)
+
+//TODO:
+router.route('/:userId/friends').get(getAllFriends)
 
 router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend)
 
